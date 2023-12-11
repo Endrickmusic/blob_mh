2fd426b6399140bbe983fb99fb9257f848cc15f7
Christian Hohenbild <mail@christianhohenbild.de>
2023-12-11 18:48:21 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,68 @@
-import { useState } from 'react'
-import Logo from '/face-blowing-a-kiss.svg'
-import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
-import Experience from "./Experience";
-import './index.css'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import { MathUtils } from "three";
+import './index.css';
 
-export default function App() {
-  
-  const [count, setCount] = useState(0)
+import vertexShader from './shader/vertexShader';
+import fragmentShader from './shader/fragmentShader';
 
- return (
+const Blob = () => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  const hover = useRef(false);
 
-  
-    <Canvas shadows camera={{ position: [0, 0, 2], fov: 40 }}>
-        <color 
-          attach="background" 
-          args={["#aaefef"]} />
-      <Experience />
-    </Canvas>
-  
+  const uniforms = useMemo(
+    () => ({
+      u_intensity: {
+        value: 0.3,
+      },
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    []
   );
-}
 
+  useFrame((state, delta) => {
+    const { clock } = state;
+    // mesh.current.material.uniforms.u_time.value = 0.4 * clock.getElapsedTime();
+    mesh.current.material.uniforms.u_time.value += delta / 3.
+
+    mesh.current.material.uniforms.u_intensity.value = MathUtils.lerp(
+      mesh.current.material.uniforms.u_intensity.value,
+      hover.current ? 0.85 : 0.15,
+      0.02
+    );
+  });
+
+  return (
+    <mesh
+      ref={mesh}
+      position={[0, 0, 0]}
+      scale={1.5}
+      onPointerOver={() => (hover.current = true)}
+      onPointerOut={() => (hover.current = false)}
+    >
+      <icosahedronGeometry args={[2, 16]} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+        wireframe={true}
+      />
+    </mesh>
+  );
+};
+
+const Scene = () => {
+  return (
+    <Canvas camera={{ position: [0.0, 0.0, 8.0] }}>
+      <Blob />
+      {/* <axesHelper /> */}
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default Scene;
